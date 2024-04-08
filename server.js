b1f20a71ddc244ab77b9757cd9a5fb93cc74f40d
a.panasenko <andrii.panasenko295@gmail.com>
2024-04-08 22:44:44 +0300

    add mongoose connection to server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const app = require("./app");
+
+const { DB_HOST, PORT = 3000 } = process.env;
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("Database connected succesfuly");
+    app.listen(PORT);
+  })
+  .catch((e) => {
+    console.log(e.message);
+    process.exit(1);
+  });
