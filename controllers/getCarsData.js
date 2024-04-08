bd7b3264c470ac0676717cd4b0626a2eb837fa64
a.panasenko <andrii.panasenko295@gmail.com>
2024-04-09 00:22:18 +0300

    add controller getCarsData

---
+++ controllers/getCarsData.js
@@ -0,0 +1,15 @@
+const controllerWrapper = requirq("../helpers");
+
+const Car = require("../models/car");
+
+const getCarsData = async (req, res) => {
+  const data = await Car.find();
+
+  if (!data) throw new Error("values not found");
+
+  res.status(200).json(data);
+};
+
+module.exports = {
+  getCarsData: controllerWrapper(getCarsData),
+};
---
+++ controllers/index.js
@@ -0,0 +1,3 @@
+const getCarsData = require('./getCarsData');
+
+module.exports = getCarsData;
