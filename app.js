0582d5bcd1935956d8152ab0ed28b658fbbdbb6b
a.panasenko <andrii.panasenko295@gmail.com>
2024-04-09 00:30:56 +0300

    uncomment middlewears

--- app.js
+++ app.js
@@ -14,13 +14,13 @@ app.use(morgan("tiny"));
 
 app.use("/api/cars", carsRouter)
 
-// app.use((_, res) => {
-//     res.status(404).json({message: "Route not found"})
-// })
+app.use((_, res) => {
+    res.status(404).json({message: "Route not found"})
+})
  
-// app.use((err, req, res, next) => {
-//     const { status = 500, message = "Server error" } = err;
-//     res.status(status).json({message})
-// })
+app.use((err, req, res, next) => {
+    const { status = 500, message = "Server error" } = err;
+    res.status(status).json({message})
+})
 
 module.exports = app;
