1265d1d8fe36a4ac85cfe5c1bc7f3b3e1953205f
a.panasenko <andrii.panasenko295@gmail.com>
2024-04-26 20:38:19 +0300

    Update getCarsData.js
    add httpError()

--- controllers/getCarsData.js
+++ controllers/getCarsData.js
@@ -1,11 +1,11 @@
-const { controllerWrapper } = require("../helpers");
+const { controllerWrapper, httpError } = require("../helpers");
 
 const Car = require("../models/car");
 
 const getCarsData = async (req, res) => {
   const carsInfo = await Car.find();
 
-  if (!carsInfo) throw new Error("values not found");
+  if (!carsInfo) throw httpError("values not found");
 
   const data = carsInfo.concat(req.cloudinaryResources);
 
